d6be61ce80990aa108c87d0d215fc19aa3abaa60
Michal Falkiewicz <michal.falkiewicz.m@gmail.com>
2020-04-08 01:59:27 +0200

    changed .java so it can run without its package

--- RAiMServer.java
+++ RAiMServer.java
@@ -1,57 +1,8 @@
-package com.company;
 
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-class ThreadServer extends  Thread
-{
-    BufferedReader input;
-    PrintStream output;
-    Socket socket;
-    String[] request= null;
-    String response = null;
-
-    public  ThreadServer(Socket socket)
-    {
-        this.socket = socket;
-    }
-
-    public void run()
-    {
-        try {
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new PrintStream(socket.getOutputStream());
-            String inputrequest = input.readLine();
-            request = inputrequest.split(" ");
-
-            //sprawdza, czy GET został odebrany poprawnie
-            if(request[0].equals("GET") && request[1].equals("/") && request[request.length-1].equals("HTTP/1.1"))
-            {
-                Date date = new Date();
-                String time_date =  new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(date);
-                response = "<html>\n<title>Wynikowa strona internetowa</title>\n<body>Odpowiedz z serwera: <br>Żądanie: "
-                        + inputrequest + "<br>Data, godzina: " + time_date + "<br>Adres IP: " + socket.getLocalAddress()
-                        + "<br>Numer portu: " + socket.getPort() + "\n</body>\n</html>";
-                System.out.println("Pomyślnie wysłano!");
-                output.println(response);
-                output.close();
-                input.close();
-            }
-            else
-            {
-                output.println("Niepoprawne żądanie!");
-                output.close();
-                input.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-}
-
 
 public class RAiMServer {
 
@@ -101,3 +52,51 @@ public class RAiMServer {
         }
     }
 }
+
+class ThreadServer extends  Thread
+{
+    BufferedReader input;
+    PrintStream output;
+    Socket socket;
+    String[] request= null;
+    String response = null;
+
+    public  ThreadServer(Socket socket)
+    {
+        this.socket = socket;
+    }
+
+    public void run()
+    {
+        try {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintStream(socket.getOutputStream());
+            String inputrequest = input.readLine();
+            request = inputrequest.split(" ");
+
+            //sprawdza, czy GET został odebrany poprawnie
+            if(request[0].equals("GET") && request[1].equals("/") && request[request.length-1].equals("HTTP/1.1"))
+            {
+                Date date = new Date();
+                String time_date =  new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(date);
+                response = "<html>\n<title>Wynikowa strona internetowa</title>\n<body>Odpowiedz z serwera: <br>Żądanie: "
+                        + inputrequest + "<br>Data, godzina: " + time_date + "<br>Adres IP: " + socket.getLocalAddress()
+                        + "<br>Numer portu: " + socket.getPort() + "\n</body>\n</html>";
+                System.out.println("Pomyślnie wysłano!");
+                output.println(response);
+                output.close();
+                input.close();
+            }
+            else
+            {
+                output.println("Niepoprawne żądanie!");
+                output.close();
+                input.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
